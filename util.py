582928d9bc6ccc6b62feb53285c4d55d6ebd5606
Khairulmizam Samsudin <xource@gmail.com>
2024-08-16 18:09:49 +0800

    feat: Add filename argument to mermaid_ink()

--- util.py
+++ util.py
@@ -1,8 +1,22 @@
 import base64
+import requests
 from IPython.display import Image, display
 
-def mermaid_ink(graph):
+def mermaid_ink(graph, filename = None):
     graphbytes = graph.encode("utf8")
     base64_bytes = base64.b64encode(graphbytes)
     base64_string = base64_bytes.decode("ascii")
-    display(Image(url="https://mermaid.ink/img/" + base64_string))
+
+    image_url = "https://mermaid.ink/img/" + base64_string
+
+    if filename:
+        # Fetch the image and save it as a PNG file
+        response = requests.get(image_url)
+        if response.status_code == 200:
+            with open(filename, 'wb') as file:
+                file.write(response.content)
+        else:
+            print(f"Failed to retrieve the image. Status code: {response.status_code}")
+    else:
+        display(Image(url=image_url))
+
